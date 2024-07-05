8dbb62f440bdf1f0c96c704b3d24ab420c149ea4
fattypanda <1076659852@qq.com>
2024-07-05 15:17:07 +0800

    调整；

---
+++ math.ts
@@ -0,0 +1,6 @@
+import { serve } from "https://deno.land/std@0.153.0/http/server.ts";
+import { serveDir } from "https://deno.land/std@0.153.0/http/file_server.ts";
+
+serve((req) => {
+	return serveDir(req, { fsRoot: "dist" });
+});
