4f9ef9551b8f43d3aa3bdf55bcbe1601fce7a6dd
fattypanda <1076659852@qq.com>
2024-07-05 15:19:31 +0800

    调整；

--- math.ts
+++ math.ts
@@ -2,5 +2,12 @@ import { serve } from "https://deno.land/std@0.153.0/http/server.ts";
 import { serveDir } from "https://deno.land/std@0.153.0/http/file_server.ts";
 
 serve((req) => {
-	return serveDir(req, { fsRoot: "dist" });
+	const url = new URL(req.url);
+	
+	// Redirect root to /index.html
+	if (url.pathname === "/") {
+		url.pathname = "/index.html";
+	}
+	
+	return serveDir(req, { fsRoot: "public", url: url.toString() });
 });
